d4cdfd8b4756cb90687133b582268968074822f4
InfV2 <skibidisigma123456@proton.me>
2025-02-07 10:59:54 -0800

    Update friends.js

--- src/routes/friends.js
+++ src/routes/friends.js
@@ -1,13 +1,33 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
+const express = require('express');
+const app = express.Router();
 
-app.get("/friends/api/v1/*/settings", (req, res) => {
-    res.status(200).send({
-        status: "OK",
-        code: 200
-    });
-});
+app.get('/friends/api/v1/:accountId/blocklist', (req, res) => {
+    console.log(req.originalUrl)
+    return res.status(200).send([])
+})
 
+app.get('/friends/api/public/blocklist/:accountId', (req, res) => {
+    console.log(req.originalUrl)
+    return res.status(200).send([])
+})
+
+app.get('/friends/api/v1/:accountId/summary', (req, res) => {
+    console.log(req.originalUrl)
+    return res.status(200).send([])
+})
+
+app.get('/friends/api/public/friends/:accountId', (req, res) => {
+    console.log(req.originalUrl)
+    return res.status(200).send([])
+})
+
+app.get('/friends/api/v1/:accountId/recent/fortnite', (req, res) => {
+    console.log(req.originalUrl)
+    return res.status(200).send([])
+})
+
+app.get('/friends/api/v1/:accountId/settings', (req, res) => {
+    console.log(req.originalUrl)
+    return res.status(200).send([])
+})
 module.exports = app;
