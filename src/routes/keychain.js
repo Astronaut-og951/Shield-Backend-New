e1dbd1258d676f46aee1a3063329b8830bb54722
InfV2 <skibidisigma123456@proton.me>
2025-02-07 09:35:43 -0800

    Update keychain.js

--- src/routes/keychain.js
+++ src/routes/keychain.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express();
 const keychain = require("../responses/keychain.json");
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.get('/fortnite/api/storefront/v2/keychain', (req, res) => {
     return res.status(200).send(keychain);
