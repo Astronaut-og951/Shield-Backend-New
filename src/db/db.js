4e2378de19af66b9f355f8a900d469d454b6b8bf
InfV2 <skibidisigma123456@proton.me>
2025-02-07 18:05:56 +0100

    Add files via upload

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.DB_URL);
+    console.log("[Mongoose] Connected");
+  } catch (err) {
+    console.log(`[Mongoose] Failed -> ${err}`);
+    process.exit(1);
+  }
+};
+
+module.exports = { connectDB };
