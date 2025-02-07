cb4d9189b68ec4b04431d736795ea53cb379b1a2
InfV2 <skibidisigma123456@proton.me>
2025-02-07 10:06:25 -0800

    move connectdb to index.js

--- index.js
+++ index.js
@@ -44,6 +44,17 @@ app.get('/', (req, res) => {
     })
 })
 
+function connectDB() {
+    try {
+        mongoose.set("strictQuery", true);
+        mongoose.connect(process.env.DB_URL);
+        console.log("[Mongoose] Connected");
+    } catch (err) {
+        console.log(`[Mongoose] Failed -> ${err}`);
+        process.exit(1);
+    }
+}
+
 function start() {
     app.listen(PORT, () => {
         console.log(`Server is running on ${PORT} ✅`);
