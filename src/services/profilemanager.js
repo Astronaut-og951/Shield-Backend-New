cac2006ab20b8fd670776ca56863ee4abd3fe12d
InfV2 <skibidisigma123456@proton.me>
2025-02-07 11:38:36 -0800

    Update profilemanager.js

--- src/services/profilemanager.js
+++ src/services/profilemanager.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
-
-function getProfilePath() {
+const athena = require("")
+function CreateProfile(accountId) {
  
 }
+
+module.exports = { CreateProfile }
