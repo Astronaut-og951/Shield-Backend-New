b98548a02fc52a973dda874cbada9925f98b6259
InfV2 <elwin.nilsson@proton.me>
2025-02-07 13:51:36 -0800

    upodaasd

--- src/api/api.js
+++ src/api/api.js
@@ -10,7 +10,12 @@ const authkey = process.env.AUTHKEY;
 app.get("/lexia/api/login", async (req, res) => {
     const { email, password } = req.query;
     try {
-        
+        res.status(200).json({
+            success: true,
+            email: email,
+            password: password,
+        });
+        /*
         if (!email) return res.status(400).send({ error: "Email is required" });
         if (!password) return res.status(400).send({ error: "Password is required" });
     
@@ -21,7 +26,7 @@ app.get("/lexia/api/login", async (req, res) => {
             success: true,
             email: email,
             password: password,
-        });
+        });*/
     } catch {
         res.json(400).send({
             success: false,
