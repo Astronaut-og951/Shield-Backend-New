55a2c4da64673733d54207bd5aa9bc9c7ed0a206
InfV2 <skibidisigma123456@proton.me>
2025-02-07 09:21:29 -0800

    base ait

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,24 +1,73 @@
 const express = require("express");
 const app = express();
 
-app.get("/account/api/oauth/token", (req, res) => {
-res.status(200).send({
-    access_token: "eg1~fortnite",
-    expires_in: 28800,
-    expires_at: "2026-12-02T01:12:01.100Z",
-    token_type: "bearer",
-    refresh_token: "eg1~fortnite",
-    refresh_expires: 86400,
-    refresh_expires_at: "2026-12-02T01:12:01.100Z",
-    account_id: "fornite",
-    client_id: "fornite",
-    internal_client: true,
-    client_service: "fortnite",
-    displayName: "slurphosting",
-    app: "fortnite",
-    in_app_id: "fornite",
-    device_id: "fornite"
+app.post("/fortnite/api/game/v2/tryPlayOnPlatform/account/:accountId", (req, res) => {
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send("true");
+})
+app.get("/account/api/public/account/:accountId/externalAuths", (req, res) => {
+    res.status(200).send([]);
+})
+
+app.get("/fortnite/api/game/v2/enabled_features", (req, res) => {
+    res.status(200).send([]);
+})
+
+app.get("/content-controls/:accountId", (req, res) => {
+    res.status(200).send([]);
+})
+
+app.get("/account/api/public/account", (req, res) => {
+    res.status(200).send({
+        id: "fortnite",
+        displayName: "fortnite",
+        externalAuth: {}
+    });
+})
+
+app.get("/account/api/public/account/:accountId", (req, res) => {
+    res.status(200).send({
+        id: "fortnite",
+        displayName: "slurphosting",
+        name: "slurphosting",
+        email: "slurphosting@slurphosting.dev",
+        failedLoginAttempts: 0,
+        lastLogin: "Timestamp",
+        numberOfDisplayNameChanges: 0,
+        ageGroup: "UNKNOWN",
+        headless: false,
+        country: "US",
+        lastName: "SlurpHosting",
+        links: {},
+        preferredLanguage: "en",
+        canUpdateDisplayName: false,
+        tfaEnabled: true,
+        emailVerified: true,
+        minorVerified: false,
+        minorExpected: false,
+        minorStatus: "UNKNOWN",
     });
 });
+
+app.get("/api/v1/user/setting", (req, res) => {
+    res.status(200).send({
+        status: "OK",
+        code: 200
+    });
+})
+
+
+app.get("/socialban/api/public/v1/:accountId", (req, res) => {
+    res.status(200).send([]);
+});
+
+app.get("/presence/api/v1/_/:accountId/settings/subscriptions", (req, res) => {
+    res.status(200).send([]);
+});
+
+app.get("/fortnite/api/game/v2/privacy/account/:accountId", (req, res) => {
+    res.status(200).send([]);
+});
+
 
 module.exports = app;
