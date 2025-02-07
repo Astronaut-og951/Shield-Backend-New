5461c9496b29a863c5949ffa03b0b220df582c22
InfV2 <skibidisigma123456@proton.me>
2025-02-07 09:12:01 -0800

    set mongoose strictquery to true

--- src/db/db.js
+++ src/db/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    mongoose.set("strictQuery", true);
     mongoose.connect(process.env.DB_URL);
     console.log("[Mongoose] Connected");
   } catch (err) {
