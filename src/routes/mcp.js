67f70052b4d237403454bd9aed2eb9bce7b94574
InfV2 <skibidisigma123456@proton.me>
2025-02-07 09:36:52 -0800

    mcp fix

--- src/routes/mcp.js
+++ src/routes/mcp.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.get("/account", (req, res) => {
-res.status(200).send({
-    status: "OK",
-    code: 200
-});
+app.get("/fortnite/api/game/v2/profile/:accountId/client/:operation", (req, res) => {
+    res.status(200).send({
+        status: "OK",
+        code: 200
+    });
 });
 
 module.exports = app;
