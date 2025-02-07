4a2ddcb08b611362f265190672812d306b0837f0
InfV2 <skibidisigma123456@proton.me>
2025-02-07 09:23:49 -0800

    Update Datarouter.js my spelling is actually the worst

--- src/routes/Datarouter.js
+++ src/routes/Datarouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/datarouter", (req, res) => {
-res.status(200).send({
-    status: "OK",
-    code: 200
-});
+app.get("/datarouter/api/v1/public/data", (req, res) => {
+    res.status(200).send({
+        status: "OK",
+        code: 200
+    });
 });
 
 module.exports = app;
