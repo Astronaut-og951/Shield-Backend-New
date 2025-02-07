018a4b9cafcc62c34a1cc47e93e65f432c3cc8d7
InfV2 <skibidisigma123456@proton.me>
2025-02-07 10:46:26 -0800

    Update log.js

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,23 +1,23 @@
-class Logging {
+class log {
     static lexia(message) {
-      console.log(`\x1b[35m[LEXIA]\x1b[0m ${message}`); // Magenta
+      console.log(`\x1b[35m[LEXIA]\x1b[0m ${message}`); 
     }
   
     static debug(message) {
-      console.log(`\x1b[34m[DEBUG]\x1b[0m ${message}`); // Blue
+      console.log(`\x1b[34m[DEBUG]\x1b[0m ${message}`); 
     }
   
     static warn(message) {
-      console.log(`\x1b[33m[WARN]\x1b[0m ${message}`); // Yellow
+      console.log(`\x1b[33m[WARN]\x1b[0m ${message}`); 
     }
   
     static error(message) {
-      console.log(`\x1b[31;1m[ERROR]\x1b[0m ${message}`); // Bright Red
+      console.log(`\x1b[31;1m[ERROR]\x1b[0m ${message}`); 
     }
   
     static api(message) {
-      console.log(`\x1b[32m[API]\x1b[0m ${message}`); // Green
+      console.log(`\x1b[32m[API]\x1b[0m ${message}`); 
     }
-  }
+}
   
-  module.exports = { Logging };
+module.exports = log;
