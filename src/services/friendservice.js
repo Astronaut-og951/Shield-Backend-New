3aa2a7ada78ec7c319045ad515d281d6be4a3a9a
InfV2 <skibidisigma123456@proton.me>
2025-02-08 03:33:34 -0800

    Update friendservice.js

--- src/services/friendservice.js
+++ src/services/friendservice.js
@@ -3,8 +3,6 @@ const path = require("path");
 const Friends = require("../../db/models/friends");
 
 async function CreateFriends(accountId) {
-
-  
     await Friends.create({
       created: new Date.now(),
       accountId,
