d6d026b92a4613d502656b18ffd8f849c2bb84af
InfV2 <skibidisigma123456@proton.me>
2025-02-08 03:36:21 -0800

    Update register.js

--- src/bot/commands/register.js
+++ src/bot/commands/register.js
@@ -9,12 +9,13 @@ module.exports = {
         .addStringOption(option => option.setName('email').setDescription('Your desired email').setRequired(true))
         .addStringOption(option => option.setName('password').setDescription('Your desired safe password').setRequired(true)),
 	async execute(interaction) {
+	await interaction.deferReply();
         const username = interaction.options.getString('username');
         const email = interaction.options.getString('email');
         const password = interaction.options.getString('password');
         const discordId = interaction.user.id;
         
         functions.createAccount(username, email, password, discordId);
-	await interaction.reply({ content: 'Successfully created your account!', flags: MessageFlags.Ephemeral });
+	await interaction.reply({ content: 'Successfully created your account!' });
 	},
 };
